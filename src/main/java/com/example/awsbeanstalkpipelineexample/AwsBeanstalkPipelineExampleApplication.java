5d93bb56f4ec8a71eeb683f83b32e7e48f623de7
Asmat Soomro <soomro.asmat@gmail.com>
2020-04-11 21:20:42 -0400

    initial commit

---
+++ src/main/java/com/example/awsbeanstalkpipelineexample/AwsBeanstalkPipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsbeanstalkpipelineexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstalkPipelineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstalkPipelineExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsbeanstalkpipelineexample/AwsBeanstalkPipelineExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsbeanstalkpipelineexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstalkPipelineExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
